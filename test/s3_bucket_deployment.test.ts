c66287f5242bdf90afde3f8857056471289e2ca6
nkserveren26 <norinoritoshi@outlook.jp>
2025-04-29 16:27:46 +0900

    Initial commit

---
+++ test/s3_bucket_deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3BucketDeployment from '../lib/s3_bucket_deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_bucket_deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3BucketDeployment.S3BucketDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
