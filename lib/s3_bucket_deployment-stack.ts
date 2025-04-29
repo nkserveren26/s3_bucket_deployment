c4545ddb20e63633691d11e5a20e06e3b956a536
nkserveren26 <norinoritoshi@outlook.jp>
2025-04-29 17:12:00 +0900

    ソースコード修正。

--- lib/s3_bucket_deployment-stack.ts
+++ lib/s3_bucket_deployment-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Source, BucketDeployment } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +8,16 @@ export class S3BucketDeploymentStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 既存S3バケットを取得
+    const existing_bucket = Bucket.fromBucketArn(
+      this,
+      "cdk-s3-bucket-deployemnt-test-nk",
+      "arn:aws:s3:::cdk-s3-bucket-deployemnt-test-nk"
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3BucketDeploymentQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const uploadObjectsToS3Bucket: BucketDeployment = new BucketDeployment(this, "uploadObjectsToS3Bucket", {
+      sources: [Source.asset("bucket_objects")],
+      destinationBucket: existing_bucket,
+    });
   }
 }
